44fc45019dc0d7baf2e7a17c4f7d01f0a37bc057
Seongmin <lll2slll@naver.com>
2019-12-17 15:15:32 +0900

    post-deploy 수정

--- config/ecosystem.config.js
+++ config/ecosystem.config.js
@@ -19,7 +19,7 @@ module.exports = {
             repo: 'git@github.com:tjdals12/pm2-deploy-example.git',
             ssh_options: 'StrictHostKeyChecking=no',
             path: '/home/minz',
-            'post-deploy': 'docker build -t pm2_example .',
+            'post-deploy': 'yarn install',
         },
     },
 };
